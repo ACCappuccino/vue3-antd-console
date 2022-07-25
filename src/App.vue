db38768ddf295bccfc2febb29e12059a76dcdfd0
teacc <727871552@qq.com>
2022-07-25 14:38:48 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Author: Mocha
+ * @Date: 2022-07-25 11:48:11
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-07-25 12:36:34
+ * @Description: 
+-->
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+        <a-button type="primary">Primary Button</a-button>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script>
+export default {
+    components: {},
+};
+</script>
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Mocha
+ * @Date: 2022-07-25 11:48:11
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-07-25 14:18:16
+ * @Description:
+ */
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
